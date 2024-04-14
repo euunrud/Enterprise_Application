1c22ac68b94978db11a558b68ec6efb7d0e4d043
Eunkyy <melon0802@naver.com>
2024-04-15 03:21:14 +0900

    :sparkles: 설명 추가

--- IMDBStudent.java
+++ IMDBStudent.java
@@ -15,13 +15,13 @@ public class IMDBStudent20200974 {
 
         JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
 
-        JavaRDD<String> genres = lines.map(line -> line.split("::")[2]);
+        JavaRDD<String> genres = lines.map(line -> line.split("::")[2]); //map은 RDD transformation에 해당하며 함수를 적용한 결과 RDD를 되돌려 준다.
 
-        JavaRDD<String> allGenres = genres.flatMap(genre -> Arrays.asList(genre.split("\\|")).iterator()); 
+        JavaRDD<String> allGenres = genres.flatMap(genre -> Arrays.asList(genre.split("\\|")).iterator());
 
-        JavaPairRDD<String, Integer> genreCnts = allGenres.mapToPair(genre -> new Tuple1<>(genre, 1));
+        JavaPairRDD<String, Integer> genreCnts = allGenres.mapToPair(genre -> new Tuple1<>(genre, 1)); //기본 RDD -> pairRDD로 변환.
 
-        JavaPairRDD<String, Integer> genreWordCnts = genreCnts.reduceByKey((a, b) -> a + b);
+        JavaPairRDD<String, Integer> genreWordCnts = genreCnts.reduceByKey((a, b) -> a + b);  //reduceByKey는 동일한 Key에 대한 value의 값을 합칠 수 있는 transformation.
 
         genreWordCnts.saveAsTextFile(args[1]);
 
